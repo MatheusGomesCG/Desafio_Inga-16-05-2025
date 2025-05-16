1b2ce32847af30291163405503601c360c82aab2
Matheus Gomes F. Eusebio <matt-xp2@live.com>
2025-05-16 10:31:33 -0300

    feat/calculo-desconto-proguessivo exercicio - 6 realizado calculo para desconto de valor total dos bilhetes

---
+++ EXERCICIO_6/CalculoDescontoProgressivo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace TesteInga;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        string inputUser;
+        Console.Write("Quantidade de ingressos: ");
+        inputUser = Console.ReadLine();
+        int quantityTicket = IsInteger(inputUser);
+
+        double valueTicket = 20.00;
+        double descountTicket = 0.0;
+
+        Console.Write("Valor a pagar: R$ ");
+        if (quantityTicket <= 2)
+        {
+            Console.WriteLine($"{(valueTicket * quantityTicket).ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+        else if (quantityTicket <= 4)
+        {
+            descountTicket = 2 * quantityTicket;
+            valueTicket *= quantityTicket;
+            Console.WriteLine($"{(valueTicket - descountTicket).ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+        else
+        {
+            descountTicket = 5 * quantityTicket;
+            valueTicket *= quantityTicket;
+            Console.WriteLine($"{(valueTicket - descountTicket).ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+    static int IsInteger(string inputUser)
+    {
+        int.TryParse(inputUser, out int isIntegerValue);
+        while (isIntegerValue <= 0 || isIntegerValue > 50)
+        {
+            Console.Write("Digite um valor acima de zero e menor que 50, e igual a inteiro: ");
+            inputUser = Console.ReadLine();
+            int.TryParse(inputUser, out isIntegerValue);
+        }
+        return isIntegerValue;
+    }
+}
