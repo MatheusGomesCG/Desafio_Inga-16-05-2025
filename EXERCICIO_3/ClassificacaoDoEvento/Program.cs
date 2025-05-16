803b901ca27e28bf8a989e052cfb824ebb33e1ab
Matheus Gomes F. Eusebio <matt-xp2@live.com>
2025-05-16 09:29:36 -0300

    feat/classificacao-evento: exercicio -3 foi realizado a criação da classificacao do evento

---
+++ EXERCICIO_3/ClassificacaoDoEvento/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TesteInga;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.Write("Digite o número de participantes: ");
+        string inputUser = Console.ReadLine();
+        int quantityPeople = IsInteger(inputUser);
+
+        if (quantityPeople < 50)
+        {
+            Console.WriteLine("Evento Pequeno");
+        }
+        else if (quantityPeople <= 200)
+        {
+            Console.WriteLine("Evento Médio");
+        }
+        else
+        {
+            Console.WriteLine("Evento Grande");
+        }
+
+    }
+    static int IsInteger(string inputUser)
+    {
+        int.TryParse(inputUser, out int isIntegerValue);
+        while (isIntegerValue <= 0)
+        {
+            Console.Write("Digite um valor acima de zero e igual a inteiro: ");
+            inputUser = Console.ReadLine();
+            int.TryParse(inputUser, out isIntegerValue);
+        }
+        return isIntegerValue;
+    }
+}
